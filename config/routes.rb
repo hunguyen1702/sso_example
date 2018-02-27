c4c7603e9e3fa5447f05404b940972114839bad5
Nguyen Viet Hung <hunguyen1702@gmail.com>
2018-02-27 22:48:48 +0700

    devise saml authenticated example with Okta

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'static_pages/index'
+
+  get 'static_pages/secret'
+
+  root to: "static_pages#index"
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
